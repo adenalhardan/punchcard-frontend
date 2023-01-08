44573bd0f0b8c47976f1ecd9d10b6a50cf38b07e
Aden  Al-Hardan <adenalhardan@gmail.com>
2023-01-08 13:25:33 -0800

    before expo eject

--- App.js
+++ App.js
@@ -1,14 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {useEffect, useState} from 'react'
+import {StyleSheet, Text, SafeAreaView} from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+//import {BleManager} from 'react-native-ble-plx'
+//import Peripheral, {Service, Characteristic} from 'react-native-peripheral'
+
+//const manager = new BleManager()
+
+const App = () => {
+	const [devices, setDevices] = useState([])
+	/*
+	useEffect(() => {
+		manager.startDeviceScan(null, {allowDuplicates: false}, async (error, device) => {
+			if(error) {
+				manager.stopDeviceScan()
+			}
+
+			setDevices([...devices, device.localName])
+		})
+
+		Peripheral.onStateChanged(state => {
+			if(state === 'poweredOn') {
+				const ch = new Characteristic({
+					uuid: '...',
+					value: '...',
+					properties: ['read', 'write'],
+					permissions: ['readable', 'writeable']
+				})
+
+				const service = new Service({
+					uuid: '...',
+					characteristics: [ch]
+				})
+
+				Peripheral.addService(serivce).then(() => {
+					Peripheral.startAdvertising({
+						name: 'name from API',
+						serviceUuids: ['...']
+					})
+				})
+			}
+		})
+	}, [])
+	*/
+	return (
+		<SafeAreaView style = {styles.container}>
+			<Text>Devices:</Text>
+			
+			{devices.map(device => (
+				<Text key = {device}>{device}</Text>
+			))}
+		</SafeAreaView>
+	)
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
